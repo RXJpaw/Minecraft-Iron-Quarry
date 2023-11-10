7ac7b77a2f6e8eecd59e30ae3ddfec44bdffc768
RXJpaw <github@rxj.pw>
2023-11-10 12:34:56 +0100

    ✨ New Elocin color?!

--- src/main/java/pw/rxj/iron_quarry/types/DynamicText.java
+++ src/main/java/pw/rxj/iron_quarry/types/DynamicText.java
@@ -3,12 +3,14 @@ package pw.rxj.iron_quarry.types;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import pw.rxj.iron_quarry.util.ColorGradient;
 
 import java.awt.*;
 
 public enum DynamicText {
     EMPTY(0, "empty"),
-    RAINBOW(1, "rainbow");
+    RAINBOW(1, "rainbow"),
+    ELOCIN(2, "elocin");
 
     private final int id;
     private final String name;
@@ -31,6 +33,7 @@ public enum DynamicText {
     }
 
     public MutableText getText(Text text, int offset) {
+        long time = System.currentTimeMillis() / 50L;
         String string = text.getString();
 
         switch (this) {
@@ -39,13 +42,25 @@ public enum DynamicText {
             }
 
             case RAINBOW -> {
-                long time = System.currentTimeMillis() / 50L;
-
                 MutableText rainbowName = Text.empty();
 
                 for (int i = 0; i < string.length(); i++) {
                     double hue = 1.0/90.0 * (time - i - offset);
                     Style style = Style.EMPTY.withColor(Color.HSBtoRGB((float) (hue % 360), 0.5F, 1.0F));
+
+                    rainbowName.append(Text.literal(String.valueOf(string.charAt(i))).setStyle(style));
+                }
+
+                return rainbowName;
+            }
+
+            case ELOCIN -> {
+                MutableText rainbowName = Text.empty();
+                ColorGradient gradient = new ColorGradient(new Color(71, 119, 76), new Color(102, 69, 124));
+
+                for (int i = 0; i < string.length(); i++) {
+                    double hue = (time - i - offset) % 45.0;
+                    Style style = Style.EMPTY.withColor(gradient.getColor((float) hue / 22.5F).getRGB());
 
                     rainbowName.append(Text.literal(String.valueOf(string.charAt(i))).setStyle(style));
                 }
