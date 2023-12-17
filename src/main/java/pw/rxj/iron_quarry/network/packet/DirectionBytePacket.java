0fcb002976743128598f544ef4fed5f44dcfb663
RXJpaw <github@rxj.pw>
2023-12-17 18:03:21 +0100

    🎨 Fixed indentation.

--- src/main/java/pw/rxj/iron_quarry/network/packet/DirectionBytePacket.java
+++ src/main/java/pw/rxj/iron_quarry/network/packet/DirectionBytePacket.java
@@ -6,30 +6,30 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
 
 public class DirectionBytePacket {
-        public final Direction direction;
-        public final byte data;
+    public final Direction direction;
+    public final byte data;
 
-        private DirectionBytePacket(Direction direction, byte data) {
-            this.direction = direction;
-            this.data = data;
-        }
+    private DirectionBytePacket(Direction direction, byte data) {
+        this.direction = direction;
+        this.data = data;
+    }
 
-        public static DirectionBytePacket read(PacketByteBuf buf) {
-            try {
-                Direction direction = Direction.byId(buf.readByte());
-                byte data = buf.readByte();
+    public static DirectionBytePacket read(PacketByteBuf buf) {
+        try {
+            Direction direction = Direction.byId(buf.readByte());
+            byte data = buf.readByte();
 
-                return new DirectionBytePacket(direction, data);
-            } catch(Exception ignored) {
-                return null;
-            }
-        }
-        public static PacketByteBuf write(Identifier channel, Direction direction, byte data) {
-            PacketByteBuf packet = new PacketByteBuf(Unpooled.buffer());
-            packet.writeIdentifier(channel);
-            packet.writeByte(direction.getId());
-            packet.writeByte(data);
-
-            return packet;
+            return new DirectionBytePacket(direction, data);
+        } catch(Exception ignored) {
+            return null;
         }
     }
+    public static PacketByteBuf write(Identifier channel, Direction direction, byte data) {
+        PacketByteBuf packet = new PacketByteBuf(Unpooled.buffer());
+        packet.writeIdentifier(channel);
+        packet.writeByte(direction.getId());
+        packet.writeByte(data);
+
+        return packet;
+    }
+}
